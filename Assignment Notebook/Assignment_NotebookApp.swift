15299e6cc3a25bf78bbfe5d9b42a80bd8d4482b8
Joseph <jjphillipse4@gmail.com>
2026-02-04 08:50:25 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by joseph phillips on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
